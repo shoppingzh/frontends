733315782c574d11afd0b226d89030a70a87c3f3
shoppingzh <1049262057@qq.com>
2020-01-14 20:44:25 +0800

    增加引导页案例

--- vue/study-main/src/App.vue
+++ vue/study-main/src/App.vue
@@ -1,7 +1,12 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <transition name="fade">
+      <HelloWorld v-if="show" msg="Welcome to Your Vue.js App"/>
+    </transition>
+    <div>
+      <button @click="show = !show">切换</button>
+    </div>
   </div>
 </template>
 
@@ -12,6 +17,11 @@ export default {
   name: 'app',
   components: {
     HelloWorld
+  },
+  data() {
+    return {
+      show: false
+    }
   }
 }
 </script>
@@ -25,4 +35,12 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+.fade-enter-active, .fade-leave-active {
+  transition: opacity 5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
+
 </style>

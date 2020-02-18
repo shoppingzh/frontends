3e7bdc88c477e57ba5795343a1700844272976ce
shoppingzh <1049262057@qq.com>
2020-02-18 15:22:51 +0800

    update bui

--- bui/ui/pages/ui/startTest.js
+++ bui/ui/pages/ui/startTest.js
@@ -7,6 +7,24 @@ loader.define(function(require,exports,module){
             loading.hide();
             router.$(".main").hide();
             router.$(".blur-wrapper").removeClass('blur-wrapper');
+
+            setTimeout(() => {
+                bui.dialog({
+                    render: true
+                }).create({
+                    title: '提示',
+                    content: `
+                        <div class="container-xy">
+                            考试时间到，已自动提交。
+                        </div>
+                    `,
+                    autoClose: false,
+                    appendTo: router.$(".bui-page"),
+                    callback: function(){
+                        router.back();
+                    }
+                }).open();
+            }, 1000);
         }, 2000);
     }, { text: '正在开始..' });
 

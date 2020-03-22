b2b6758585990f5667c1c6c242b10bbf8008266a
shoppingzh <1049262057@qq.com>
2020-03-22 16:30:48 +0800

    优化密码校验算法

--- demos/password-validate/validator.js
+++ demos/password-validate/validator.js
@@ -4,8 +4,9 @@
  * @param {String} pattern  标准模式
  * @param {Number} minCount 最低连续次数
  * @param {Boolean} ignoreCase 是否忽略大小写
+ * @param {Boolean} reverse 是否忽略正反序
  */
-function isSerial(str, pattern, minCount, ignoreCase) {
+function isSerial(str, pattern, minCount, ignoreCase, reverse) {
   if (ignoreCase) {
     str = str.toLowerCase();
     pattern = pattern.toLowerCase();
@@ -22,16 +23,22 @@ function isSerial(str, pattern, minCount, ignoreCase) {
     if (i > len - minCount) {
       break;
     }
-    // 找到匹配的第一字符，连续向后匹配minCount-1次，如果都匹配则说明找到了
-    var count = 1;
+    // 找到匹配的第一字符，连续向后(或向前)匹配minCount-1次，如果都匹配则说明找到了
+    var count = 1, reverseCount = 1;
     for (var j = 1; j < minCount; j++) {
-      var m = str[i + j], n = pattern[idx + j];
-      if (m !== n) {
+      var m = str[i + j], n = pattern[idx + j], rn = pattern[idx - j];
+      // 这里不是必须break，但是为了性能，当前后都没找到时，肯定不满足，直接退出循环
+      if (m !== n && (reverse && m !== rn)) {
         break;
       }
-      count++;
+      if (m === n) {
+        count++;
+      }
+      if (m === rn) {
+        reverseCount++;
+      }
     }
-    if (count >= minCount) {
+    if (count >= minCount || (reverse && reverseCount >= minCount)) {
       serial = true;
       break;
     }

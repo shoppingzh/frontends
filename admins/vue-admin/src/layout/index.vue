575072b690f7a0302267712ff45ea5f3fac79a0e
shoppingzh <1049262057@qq.com>
2020-08-05 23:04:34 +0800

    顶部navbar固定

--- admins/vue-admin/src/layout/index.vue
+++ admins/vue-admin/src/layout/index.vue
@@ -1,21 +1,27 @@
 <template>
-  <div class="layout">
+  <section class="layout layout--fixed">
     <aside class="layout__aside">
       <el-scrollbar>
-        <el-menu>
-          <el-menu-item>控制台</el-menu-item>
-          <el-menu-item>控制台</el-menu-item>
-          <el-menu-item>控制台</el-menu-item>
-          <el-menu-item>控制台</el-menu-item>
-          <el-menu-item>控制台</el-menu-item>
+        <el-menu default-active="0">
+          <el-menu-item index="0">控制台</el-menu-item>
+          <el-menu-item index="1">控制台</el-menu-item>
+          <el-menu-item index="2">控制台</el-menu-item>
+          <el-menu-item index="3">控制台</el-menu-item>
+          <el-menu-item index="4">控制台</el-menu-item>
         </el-menu>
       </el-scrollbar>
     </aside>
-    <main class="layout__main" />
-  </div>
+    <main class="layout__main">
+      <nav class="layout__navbar"></nav>
+      <div class="layout__content"></div>
+    </main>
+  </section>
 </template>
 
 <script>
 export default {
+  create() {
+    
+  }
 }
 </script>

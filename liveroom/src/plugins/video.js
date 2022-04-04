7883e0dbcd3e97357128e75d7dbb2e6972306e43
shoppingzh <1049262057@qq.com>
2022-04-04 16:23:36 +0800

    clear unuseful code

--- liveroom/src/plugins/video.js
+++
@@ -1,8 +0,0 @@
-import 'video.js/dist/video-js.min.css'
-import videojs from 'video.js'
-import language from 'video.js/dist/lang/zh-CN.json'
-import 'videojs-flash'
-
-videojs.addLanguage('zh-CN', language)
-
-export default videojs
